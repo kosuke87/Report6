5445b2ad1c80a4d1a0240bef949ebc8f32dcd4ed
Kosuke Hanashiro <e175757@ie.u-ryukyu.ac.jp>
2018-01-15 19:51:30 +0900

    土台

---
+++ src/main/java/jp/ac/uryukyu/ie/e175757/Main.java
@@ -0,0 +1,11 @@
+package jp.ac.uryukyu.ie.e175757;
+import java.util.*;
+public class Main  {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int player1=sc.nextInt();
+        int player2=sc.nextInt();
+
+    }
+
+}
